ead59b0e25661f08c070d166d1a3a0ef19588bea
Samim Aktar <samimaktar0773@gmail.com>
2023-11-17 23:34:24 +0530

    Change background color on button click

---
+++ DAY_1_Changes_background_color/index.js
@@ -0,0 +1,14 @@
+function changeBackgroundColor() {
+  let body = document.getElementById("body");
+  body.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  console.log(color);
+  return color;
+}
